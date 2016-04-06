34fccbc7f928dd1d5b78ef2641e82c2091acc2fa
Jim Lin <jim_lin@quantatw.com>
2016-04-06 10:01:28 +0800

    Change "from import" to import for good convention
    Signed-off-by: Jim Lin <jim_lin@quantatw.com>

--- ey3.py
+++ ey3.py
@@ -1,6 +1,5 @@
 #
-from openpyxl import Workbook
-from openpyxl.chart import BarChart, Reference
+import openpyxl
 import glob
 
 FOLDER_PREFIX_NAME = "EY"
@@ -25,7 +24,7 @@ def find_between_r(s, first, last):
 
 
 def excel_create():
-    wb = Workbook()
+    wb = openpyxl.Workbook()
     ws = wb.active
     # remove the sheet named "Sheet"
     wb.remove_sheet(ws)
@@ -79,16 +78,17 @@ def excel_creatsheet(wb, ws_title):
 
 
 def excel_create_barchart(ws):
-    chart1 = BarChart()
+    chart1 = openpyxl.chart.BarChart()
     chart1.type = "col"
     chart1.style = 10
     chart1.title = "Bar Chart"
     chart1.y_axis.title = 'Test number'
     chart1.x_axis.title = 'Sample length (mm)'
 # Select all data include title
-    data = Reference(ws, min_col=2, min_row=1, max_row=19, max_col=2)
+    data = openpyxl.chart.Reference(ws, min_col=2, min_row=1,
+                                    max_row=19, max_col=2)
 # Select data only
-    cats = Reference(ws, min_col=1, min_row=2, max_row=19)
+    cats = openpyxl.chart.Reference(ws, min_col=1, min_row=2, max_row=19)
     chart1.add_data(data, titles_from_data=True)
     chart1.set_categories(cats)
     chart1.shape = 4
@@ -107,4 +107,5 @@ if __name__ == '__main__':
         ws_sn = excel_creatsheet(wb, ey[:-1])
         roi_mtp_dealwith(ws_sn, ey)
         sfr_dealwith(ws_sn, ey)
+        excel_create_barchart(ws_sn)
     excel_save(wb, "sample.xlsx")
