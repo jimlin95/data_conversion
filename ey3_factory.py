a43ca9621d00b1751c11eb12b520ce7ad54a05d2
Jim Lin <jim_lin@quantatw.com>
2016-04-21 19:56:31 +0800

    Modified directory structure & parse data by following sequence chart_out color_fidelity mtf-H-MTFOUT mtf-V-MTFOUT SFROUT_shopfloor tp1-black-out BEY36616005J-diffuser-1-5000
    Signed-off-by: Jim Lin <jim_lin@quantatw.com>

--- ey3_factory.py
+++ ey3_factory.py
@@ -24,28 +24,27 @@ VERSION = (0, 2)
 # Maintainer <jim_lin@quantatw.com>
 
 
-FOLDER_PREFIX_NAME = "*EY3_"
-
 FILENAME = "ey3_factory.xlsx"
-MTF_PATH = ""
-SFR_PATH = ""
-TP1_PATH = ""
-TP2_PATH = ""
 ROW_OFFSET = 1
-MTF_START_POS1 = 2
+CHART_OUT_POS = 2
+CHART_OUT_LEN = 13
+COLOR_FIDELITY_START_POS = (CHART_OUT_POS + CHART_OUT_LEN)
+COLOR_FIDELITY_LEN = 12
+MTF_START_POS1 = (COLOR_FIDELITY_START_POS + COLOR_FIDELITY_LEN)
 MTF_1_LEN = 9
 MTF_START_POS2 = (MTF_START_POS1 + MTF_1_LEN)
 MTF_2_LEN = 9
 SFR_START_POS = (MTF_START_POS2 + MTF_2_LEN)
 SFR_LEN = 36
-TP1_START_POS1 = (SFR_START_POS + SFR_LEN)
-TP1_1_LEN = 3
-TP1_START_POS2 = (TP1_START_POS1 + TP1_1_LEN)
-TP1_2_LEN = 42
-TP2_START_POS = (TP1_START_POS2 + TP1_2_LEN)
-TP2_LEN = 13
-COLOR_FIDELITY_START_POS = (TP2_START_POS + TP2_LEN)
-COLOR_FIDELITY_LEN = 12
+BLACK_START_POS = (SFR_START_POS + SFR_LEN)
+BLACK_LEN = 3
+DIFFUSER_1_5000_POS = (BLACK_START_POS + BLACK_LEN)
+DIFFUSER_1_5000_LEN = 42
+DIFFUSER_2_3000_POS = (DIFFUSER_1_5000_POS + DIFFUSER_1_5000_LEN)
+DIFFUSER_2_3000_LEN = 42
+CAMERA_STATION_1 = "Camera1"
+CAMERA_STATION_2 = "Camera2"
+MATCH_PATTEN = CAMERA_STATION_1 + "/*-black-0-/*"
 
 
 def excel_create():
@@ -68,8 +67,8 @@ def find_directoies_with_substring(ey):
 
 
 def mtf_dealwith(ws, ey_folder, ey_folder_idx):
-    ws.cell(column=1, row=ey_folder_idx + ROW_OFFSET, value=ey_folder[:-1])
-    filefullpath = ey_folder + MTF_PATH + ey_folder[:-1] + "-H-MTFOUT.txt"
+    filefullpath = CAMERA_STATION_2 + "/" + ey_folder + \
+        "-chart-0-/mtf-H-MTFOUT.txt"
     with open(filefullpath, "r") as f:
         for mtf_idx, line in enumerate(f, MTF_START_POS1):
             roi, mtf = line.split('=')
@@ -77,7 +76,8 @@ def mtf_dealwith(ws, ey_folder, ey_folder_idx):
                 ws.cell(column=mtf_idx, row=1, value=roi)
             ws.cell(column=mtf_idx, row=ey_folder_idx + ROW_OFFSET,
                     value=float(mtf))
-    filefullpath = ey_folder + MTF_PATH + ey_folder[:-1] + "-V-MTFOUT.txt"
+    filefullpath = CAMERA_STATION_2 + "/" + ey_folder + \
+        "-chart-0-/mtf-V-MTFOUT.txt"
     with open(filefullpath, "r") as f:
         for mtf_idx, line in enumerate(f, MTF_START_POS2):
             roi, mtf = line.split('=')
@@ -88,7 +88,8 @@ def mtf_dealwith(ws, ey_folder, ey_folder_idx):
 
 
 def sfr_dealwith(ws, ey_folder, ey_folder_idx):
-    filefullpath = ey_folder + SFR_PATH + "SFROUT_shopfloor.txt"
+    filefullpath = CAMERA_STATION_2 + "/" + ey_folder + \
+        "-chart-0-/SFROUT_shopfloor.txt"
     with open(filefullpath, "r") as f:
         for sfr_idx, line in enumerate(f, SFR_START_POS):
             roi, sfr = line.split('=')
@@ -98,38 +99,21 @@ def sfr_dealwith(ws, ey_folder, ey_folder_idx):
                     value=float(sfr))
 
 
-def tp1_dealwith(ws, ey_folder, ey_folder_idx):
-    filefullpath = ey_folder + TP1_PATH + "tp1-black-out.txt"
+def chart_out_dealwith(ws, ey_folder, ey_folder_idx):
+    filefullpath = CAMERA_STATION_2 + "/" + ey_folder + \
+        "-chart-0-/chart-out.txt"
     with open(filefullpath, "r") as f:
-        for tp1_idx, line in enumerate(f, TP1_START_POS1):
-            roi, black = line.split('=')
+        for co_idx, line in enumerate(f, CHART_OUT_POS):
+            roi, co = line.split('=')
             if ey_folder_idx == 1:
-                ws.cell(column=tp1_idx, row=1, value=roi)
-            ws.cell(column=tp1_idx, row=ey_folder_idx + ROW_OFFSET,
-                    value=float(black))
-    filefullpath = ey_folder + TP1_PATH + "tp1-white-out.txt"
-    with open(filefullpath, "r") as f:
-        for tp1_idx, line in enumerate(f, TP1_START_POS2):
-            roi, white = line.split('=')
-            if ey_folder_idx == 1:
-                ws.cell(column=tp1_idx, row=1, value=roi)
-            ws.cell(column=tp1_idx, row=ey_folder_idx + ROW_OFFSET,
-                    value=float(white))
-
-
-def tp2_dealwith(ws, ey_folder, ey_folder_idx):
-    filefullpath = ey_folder + TP2_PATH + "chart-out.txt"
-    with open(filefullpath, "r") as f:
-        for tp2_idx, line in enumerate(f, TP2_START_POS):
-            roi, tp2 = line.split('=')
-            if ey_folder_idx == 1:
-                ws.cell(column=tp2_idx, row=1, value=roi)
-            ws.cell(column=tp2_idx, row=ey_folder_idx + ROW_OFFSET,
-                    value=float(tp2))
+                ws.cell(column=co_idx, row=1, value=roi)
+            ws.cell(column=co_idx, row=ey_folder_idx + ROW_OFFSET,
+                    value=float(co))
 
 
 def color_fidelity_dealwith(ws, ey_folder, ey_folder_idx):
-    filefullpath = ey_folder + "color_fidelity.txt"
+    filefullpath = CAMERA_STATION_2 + "/" + ey_folder + \
+        "-chart-0-/color_fidelity.txt"
     with open(filefullpath, "r") as f:
         for cfd_idx, line in enumerate(f, COLOR_FIDELITY_START_POS):
             roi, cfd = line.split('=')
@@ -139,14 +123,58 @@ def color_fidelity_dealwith(ws, ey_folder, ey_folder_idx):
                     value=float(cfd))
 
 
+def black_0_dealwith(ws, ey_folder, ey_folder_idx):
+
+    ws.cell(column=1, row=ey_folder_idx + ROW_OFFSET, value=ey_folder)
+    filefullpath = CAMERA_STATION_1 + "/" + ey_folder + \
+        "-black-0-/tp1-black-out.txt"
+    with open(filefullpath, "r") as f:
+        for black_idx, line in enumerate(f, BLACK_START_POS):
+            roi, black = line.split('=')
+            if ey_folder_idx == 1:
+                ws.cell(column=black_idx, row=1, value=roi)
+            ws.cell(column=black_idx, row=ey_folder_idx + ROW_OFFSET,
+                    value=float(black))
+
+
+def diffuser_1_5000_dealwith(ws, ey_folder, ey_folder_idx):
+
+    filefullpath = CAMERA_STATION_1 + "/" + ey_folder + \
+        "-diffuser-1-5000/tp1-white-out.txt"
+    with open(filefullpath, "r") as f:
+        for du1_idx, line in enumerate(f, DIFFUSER_1_5000_POS):
+            roi, du1 = line.split('=')
+            if ey_folder_idx == 1:
+                ws.cell(column=du1_idx, row=1, value=roi)
+            ws.cell(column=du1_idx, row=ey_folder_idx + ROW_OFFSET,
+                    value=float(du1))
+
+
+def diffuser_2_3000_dealwith(ws, ey_folder, ey_folder_idx):
+
+    filefullpath = CAMERA_STATION_1 + "/" + ey_folder + \
+        "-diffuser-2-3000/tp1-white-out.txt"
+    with open(filefullpath, "r") as f:
+        for du2_idx, line in enumerate(f, DIFFUSER_2_3000_POS):
+            roi, du2 = line.split('=')
+            if ey_folder_idx == 1:
+                ws.cell(column=du2_idx, row=1, value=roi)
+            ws.cell(column=du2_idx, row=ey_folder_idx + ROW_OFFSET,
+                    value=float(du2))
+
+
 if __name__ == '__main__':
+    EY_FOLDERS = []
     wb = excel_create()
     ws = wb.active
-    EY_FOLDERS = find_directoies_with_substring(FOLDER_PREFIX_NAME + "*/")
+    for ey_folder in find_directoies_with_substring(MATCH_PATTEN):
+        EY_FOLDERS.append(ey_folder[8:20])
+        # print(EY_FOLDERS)
     for ey_folder_idx, ey_folder in enumerate(EY_FOLDERS, 1):
+        chart_out_dealwith(ws, ey_folder, ey_folder_idx)
+        color_fidelity_dealwith(ws, ey_folder, ey_folder_idx)
         mtf_dealwith(ws, ey_folder, ey_folder_idx)
         sfr_dealwith(ws, ey_folder, ey_folder_idx)
-        tp1_dealwith(ws, ey_folder, ey_folder_idx)
-        tp2_dealwith(ws, ey_folder, ey_folder_idx)
-        color_fidelity_dealwith(ws, ey_folder, ey_folder_idx)
+        black_0_dealwith(ws, ey_folder, ey_folder_idx)
+        diffuser_1_5000_dealwith(ws, ey_folder, ey_folder_idx)
     excel_save(wb, FILENAME)
