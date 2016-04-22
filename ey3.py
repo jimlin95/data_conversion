7928353de914670a71538da4a77ea1a0be1e1cc5
Jim Lin <jim_lin@quantatw.com>
2016-04-22 14:14:29 +0800

    Add License and Version
    Signed-off-by: Jim Lin <jim_lin@quantatw.com>

--- ey3.py
+++ ey3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # coding=UTF-8
-#
 from openpyxl import Workbook
 from openpyxl.styles import (
         Side,
@@ -12,8 +11,29 @@ from openpyxl.styles import (
         colors
     )
 from openpyxl.chart import Reference, LineChart
-# import openpyxl
 import glob
+
+#
+# Copyright (C) 2016 Quanta Computer Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# increment this whenever we make important changes to this script
+VERSION = (0, 1)
+
+
+# Maintainer <jim_lin@quantatw.com>
+
 
 FOLDER_PREFIX_NAME = "*EY"
 FILENAME = "ey3.xlsx"
